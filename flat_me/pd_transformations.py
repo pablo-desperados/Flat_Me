efea3be204066e666c89c511fe8e700c8856dd46
Pablo Mujica <42331062+pablo-desperados@users.noreply.github.com>
2022-02-28 11:50:28 -0500

    Added csv_to_txt class and full implementation.

--- flat_me/pd_transformations.py
+++ flat_me/pd_transformations.py
@@ -1,35 +1,48 @@
 import pandas as pd
+import os
 from PyInquirer import prompt
 
-def change_column_name_prompt(column_ix,name):
-    question={
-        'type':'input',
-        'name':f"column_{column_ix}",
-        'message':f' #{column_ix} Choose the new name for the column  - {name}: - (press enter to make no changes):',
-        'default':f"{name}"
-    }
+class TransformationWrapper:
 
-    answer = prompt(question)
-    return answer[f"column_{column_ix}"]
-
-def change_column_name(df):
-    list_of_columns=df.columns.tolist()
-    new_list=[]
-    for ix,column in enumerate(list_of_columns):
-        new_val = change_column_name_prompt(ix,column)
-        new_list.append(new_val)
-    print("Processing column changes...")
-    df.columns = new_list
-    print("The names of the columns have been successfully changed!")
+    def __init__(self,df,file_obj,package):
+        self.df = df
+        self.file_obj = file_obj
+        self.package = package
 
 
+    def change_column_name_prompt(self,column_ix,name):
+        question={
+            'type':'input',
+            'name':f"column_{column_ix}",
+            'message':f' #{column_ix} Choose the new name for the column  - {name}: - (press enter to make no changes):',
+            'default':f"{name}"
+        }
 
-def ingest_transformations(package,df):
-    if package['confirm_transformations'] is False:
-        return df
+        answer = prompt(question)
+        return answer[f"column_{column_ix}"]
 
-    transformation_map={'Change Column Names':change_column_name}
+    def change_column_name(self):
+        list_of_columns=self.df.columns.tolist()
+        new_list=[]
+        for ix,column in enumerate(list_of_columns):
+            new_val = self.change_column_name_prompt(ix,column)
+            new_list.append(new_val)
+        print("Processing column changes...")
+        self.df.columns = new_list
+        print("The names of the columns have been successfully changed!")
 
-    for action in package['transformations']:
-        funct_to_call = transformation_map[action]
-        funct_to_call(df)
+    def change_file_name(self):
+        #TODO
+        print(os.path.basename(self.file_obj.path))
+
+
+    def ingest_transformations(self):
+        if self.package['confirm_transformations'] is False:
+            return self.df
+
+        transformation_map={'Change Column Names':self.change_column_name,
+                            'Change File Name':self.change_file_name}
+
+        for action in self.package['transformations']:
+            funct_to_call = transformation_map[action]
+            funct_to_call()
